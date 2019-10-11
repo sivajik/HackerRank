45ae7a9e3441a37efa9f6677625f92dce6c28798
sivajik <sivaji_kondapalli@yahoo.com>
2019-10-11 07:18:15 +0530

    FibModifiedDP

---
+++ src/com/hr/problemsolving/medium/FibModifiedDP.java
@@ -0,0 +1,29 @@
+package com.hr.problemsolving.medium;
+
+import java.math.BigInteger;
+
+public class FibModifiedDP {
+
+	public static void main(String[] args) {
+		BigInteger y = fibonacciModified(1, 1, 20);
+		System.out.println(y.toString());
+	}
+
+	static BigInteger add(BigInteger a, BigInteger b) {
+		return a.add(b.multiply(b));
+	}
+
+	static BigInteger fibonacciModified(int t1, int t2, int n) {
+		if (n == 1) {
+			return BigInteger.ZERO;
+		}
+
+		BigInteger[] dp = new BigInteger[n];
+		dp[0] = BigInteger.valueOf(t1);
+		dp[1] = BigInteger.valueOf(t2);
+		for (int i = 2; i < n; i++) {
+			dp[i] = add(dp[i - 2], dp[i - 1]);
+		}
+		return dp[n - 1];
+	}
+}
